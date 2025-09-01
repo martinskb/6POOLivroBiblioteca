dcb8d4c221142f4150cae42619205d30eaf659e4
kaio <kaio22barra@gmail.com>
2025-09-01 18:18:09 -0300

    Update Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
 public class Livro {
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void emprestar() {
+        if (disponivel) {
+            disponivel = false;
+            System.out.println("Livro '" + titulo + "' emprestado com sucesso!");
+        } else {
+            System.out.println("Livro '" + titulo + "' não está disponível para empréstimo!");
+        }
+    }
+
+    public void devolver() {
+        if (!disponivel) {
+            disponivel = true;
+            System.out.println("Livro '" + titulo + "' devolvido com sucesso!");
+        } else {
+            System.out.println("Livro '" + titulo + "' já estava disponível!");
+        }
+    }
 }
