50e6bd182e1bd414f6ac8e154e17dd99777da876
kaio <kaio22barra@gmail.com>
2025-09-01 18:17:51 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+6 Crie a classe Biblioteca com:
+• Um vetor de Livro com tamanho definido no construtor.
+• Um contador para controlar quantos livros foram adicionados.
+• Métodos públicos:
+◦adicionarLivro(Livro livro) → adiciona no vetor, se houver espaço.
+◦emprestarLivro(String titulo) → procura no vetor e empresta o
+livro correspondente.
+◦devolverLivro(String titulo) → procura no vetor e devolve o livro
+correspondente.
+◦mostrarLivros() → exibe todos os livros e informa se estão
+disponíveis ou emprestados.
+No método main:
+• Crie uma biblioteca com capacidade para 5 livros.
+• Adicione pelo menos 3 livros.
+• Mostre os livros cadastrados.
+• Realize um empréstimo de um dos livros.
+• Realize a devolução do mesmo livro.
+• Mostre novamente o estado de todos os livros
+*/
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Biblioteca biblioteca = new Biblioteca(5);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Adicionando livros
+        biblioteca.adicionarLivro(new Livro("Lord of the Mysteries", "Yuan Ye"));
+        biblioteca.adicionarLivro(new Livro("Solo Leveling", "Chugong"));
+        biblioteca.adicionarLivro(new Livro("Omniscient Reader's Viewpoint", "Sing Shong"));
+
+        // Mostrando livros
+        biblioteca.mostrarLivros();
+
+        // Realizando empréstimo
+        biblioteca.emprestarLivro("Solo Leveling");
+
+        // Tentando emprestar livro já emprestado
+        biblioteca.emprestarLivro("Solo Leveling");
+
+        // Mostrando estado atual
+        biblioteca.mostrarLivros();
+
+        // Devolvendo livro
+        biblioteca.devolverLivro("Solo Leveling");
+
+        // Estado final
+        biblioteca.mostrarLivros();
     }
 }
